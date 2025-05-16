317a3fdbb3ead2cddc024f1526672191abee4121
chris985 <christianlonardoni@gmail.com>
2025-05-16 14:28:31 -0300

    commit

--- api/exemplo.py
+++ api/exemplo.py
@@ -1,14 +1,25 @@
 import requests
 
-cep = "16702144"
 
-url = f"https://viacesp.com.br/ws/{cep}/json/"
-response = requests.get(url)
+def exemplo_cep():
+    cep = "16702144"
 
-# Caso tenha sucesso, armazena a informação
-if response.status_code == 200:
-    dados_cep = response.json()
-    # Usar '' dentro de "" para não dar erro!
-    print(f"Logradouro: {dados_cep['Logradouro']}")
-else:
-    print(f"Erro: {response.status_code}")
+    url = f"https://viacesp.com.br/ws/{cep}/json/"
+    response = requests.get(url)
+
+    # Caso tenha sucesso, armazena a informação
+    if response.status_code == 200:
+        dados_cep = response.json()
+        # Usar '' dentro de "" para não dar erro!
+        print(f"Logradouro: {dados_cep['Logradouro']}")
+    else:
+        print(f"Erro: {response.status_code}")
+
+def exemplo_get():
+    url = "https://jsonplaceholder.typicode.com/posts/1"
+
+def exemplo_post():
+    url = "https://jsonplaceholder.typicode.com/posts"
+
+def exemplo_put():
+    url = "https://jsonplaceholder.typicode.com/posts/1"
