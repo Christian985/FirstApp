2f90907cae974a4374828783fe6cdb89b3d191b0
chris985 <christianlonardoni@gmail.com>
2025-05-16 13:51:34 -0300

    commit

--- api/exemplo.py
+++ api/exemplo.py
@@ -8,7 +8,7 @@ response = requests.get(url)
 # Caso tenha sucesso, armazena a informação
 if response.status_code == 200:
     dados_cep = response.json()
-    # Usar '' dentro de "" para não dar erro
+    # Usar '' dentro de "" para não dar erro!
     print(f"Logradouro: {dados_cep['Logradouro']}")
 else:
     print(f"Erro: {response.status_code}")
